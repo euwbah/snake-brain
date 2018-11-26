412d1969e2d8436275e5c03f862e46c7dc10d2f4
euwbah <euwbah@gmail.com>
2018-11-26 18:07:27 +0800

    Training f(x) = x^2 + 1 on new architecture

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ if __name__ == "__main__":
 
     # d = generate_data(10)
 
-    # test_model('fx=x2+1 2-100-0.5 decay0', 2, 100, 0.5)
+    # test_model('fx=x2+1 2-30-0.5 decay0', 2, 30, 0.5)
 
-    train_model('fx=x2+1 2-100-0.5 decay3e-6',
-                2, 100, 0.5, 100, 1,
+    train_model('fx=x2+1 2-30-0.5 decay3e-6',
+                2, 30, 0.5, 100, 1,
                 0, 1, 0, 1,
                 -6, 6,
                 -8, 8,
                 60000, 4000, 300, 75,
-                step_size=0.0001, momentum=0.4, decay=0.0001,
-                log_level=1,
+                step_size=0.0003, momentum=0.4, decay=0.0001,
+                log_level=0,
                 pause_after_iter=None)
 
     exit(0)
